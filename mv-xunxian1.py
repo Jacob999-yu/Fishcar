c694b1abf556be1c39df1542da17a747d6b64298
Jacob999-yu <70765803+Jacob999-yu@users.noreply.github.com>
2021-03-24 23:04:39 +0800

    Update mv-xunxian1.py

--- mv-xunxian1.py
+++ mv-xunxian1.py
@@ -1,16 +1,16 @@
 #THRESHOLD = (5, 70, -23, 15, -57, 0) # Grayscale threshold for dark things...
 #THRESHOLD = (70, 100, -15, 15, -57, 0)
-THRESHOLD = (70, 100, -15, 15,-20,0)
+THRESHOLD = (90, 100, -20, 6,-7,15)
 import sensor, image, time
 from pyb import LED
+from pyb import UART
+from pid import PID
 #import car
 #from pid import PID
-#rho_pid = PID(p=0.4, i=0)
-#theta_pid = PID(p=0.001, i=0)
+rho_pid = PID(p=0.4, i=0)
+theta_pid = PID(p=0.001, i=0)
 
-#LED(1).on()
-#LED(2).on()
-#LED(3).on()
+
 
 sensor.reset()#chu shi hua
 #sensor.set_vflip(True)#fanzhuan
@@ -23,13 +23,14 @@ sensor.set_auto_whitebal(False)
 #sensor.set_windowing([0,20,80,40])
 sensor.skip_frames(time = 2000)     # WARNING: If you use QQVGA it may take seconds
 clock = time.clock()                # to process a frame sometimes.
+uart = UART(3, 9600)
 
-while(True):
+while True:
     clock.tick()
     img1 = sensor.snapshot()
-    print(img1)
+    #print(img1)
     img2 = img1.binary([THRESHOLD])
-    print(img2)
+    #print(img2)
     #input()
     line = img2.get_regression([(70,100)], robust = False)
     #line = img.get_regression([(100,100,0,0,0,0)], robust = True)
@@ -41,21 +42,24 @@ while(True):
         if line.theta()>90:
             theta_err = line.theta()-180
         else:
-            theta_err = line.theta()j
-        #img.draw_line(line.line(), color = 127)
-        print(rho_err,line.magnitude(),rho_err)
-        if line.magnitude()>8:
-            print(2)
+            theta_err = line.theta()
+        img2.draw_line(line.line(), color = 127)
+        #print(rho_err,line.magnitude(),rho_err)
+
+        if line.magnitude()<4:
+            #print(2)
             #if -40<b_err<40 and -30<t_err<30:
-            #rho_output = rho_pid.get_pid(rho_err,1)
-            #theta_output = theta_pid.get_pid(theta_err,1)
-            #output = rho_output+theta_output
+            rho_output = rho_pid.get_pid(rho_err,1)
+            theta_output = theta_pid.get_pid(theta_err,1)
+            output = rho_output+theta_output
+            print(output)
+            uart.write(str(output)+'\n')
             #car.run(50+output, 50-output)
         else:
-            print(0)
+            uart.write(str(0)+'\n')
             #car.run(0,0)
-    else:
-        print(5)
-        #car.run(50,-50)
-        pass
+     #else:
+       # print(0)
+    #     #car.run(50,-50)
+    #     pass
     #print(clock.fps())
